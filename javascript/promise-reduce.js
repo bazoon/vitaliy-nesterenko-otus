999dc5e52de387d933334fc0273ee8fb9307e0c0
Vitaliy <Nesterenko>
2019-04-08 16:16:20 +0500

    Доработка

--- javascript/promise-reduce.js
+++ javascript/promise-reduce.js
@@ -21,6 +21,25 @@ async function promiseReduce2(asyncFunctions, reduce, initialValue) {
   return newResult;
 }
 
+async function promiseReduce3(asyncFunctions, reduce, initialValue) {
+  return asyncFunctions.reduce(async function(acc, fn) {
+    const nextValue = await fn();
+    const accumulator = await acc;
+    return Promise.resolve(reduce(accumulator, nextValue));
+  }, Promise.resolve(initialValue));
+}
+
+async function promiseReduce4(asyncFunctions, reduce, initialValue) {
+  let newResult = Promise.resolve(initialValue);
+  asyncFunctions.forEach(function(fn) {
+    newResult = newResult.then(async initial => {
+      return reduce(initial, await fn());
+    });
+  });
+
+  return newResult;
+}
+
 const fn1 = () => {
   console.log("fn1");
   return Promise.resolve(1);
@@ -32,7 +51,7 @@ const fn2 = () =>
     setTimeout(() => resolve(2), 1000);
   });
 
-promiseReduce(
+promiseReduce4(
   [fn1, fn2],
   (acc, e) => {
     console.log("reduce");
