e7da41f79a373e08d7a39882b68de04a1540a827
Vitaliy <Nesterenko>
2019-04-23 19:59:53 +0500

    Правки по замечаниям

--- node3/tree.js
+++ node3/tree.js
@@ -1,47 +1,41 @@
 const fs = require("fs");
-const path = require("path");
 const util = require("util");
+const readdirP = util.promisify(fs.readdir);
 
 let folder = process.argv[2];
-walkFiles(folder);
 
-function walkFiles(folder, onSuccess) {
-  if (!folder) {
-    console.log("Укажите путь до папки!");
-    process.exit(1);
-  }
-
-  if (folder.endsWith("/")) {
-    folder = folder.slice(0, -1);
-  }
-  let tree = {};
-  walk(folder, tree).then(r => {
-    console.log(tree);
-  });
+if (!folder) {
+  console.log("Укажите путь до папки!");
+  process.exit(1);
 }
 
-function walk(dir, tree) {
-  tree.dirs = tree.dirs || [];
-  tree.files = tree.files || [];
-  tree.dirs.push(dir);
-  return new Promise((resolve, reject) => {
-    // Требуется node версии 10.15
-    fs.readdir(dir, { withFileTypes: true }, (err, list) => {
-      if (!err) {
-        const promises = list.map(f => {
-          const fullPath = `${dir}/${f.name}`;
+if (folder.endsWith("/")) {
+  folder = folder.slice(0, -1);
+}
 
-          if (f.isDirectory()) {
-            return walk(fullPath, tree);
-          } else {
-            tree.files.push(fullPath);
-            return Promise.resolve();
-          }
-        });
-        return resolve(Promise.all(promises));
-      } else {
-        return reject(err);
-      }
-    });
+walk(folder).then(r => {
+  console.log(r);
+});
+
+async function walk(dir, tree = { dirs: [], files: [] }) {
+  const fileList = await readdirP(dir, { withFileTypes: true });
+  const promises = fileList.map(async file => {
+    const fullPath = `${dir}/${file.name}`;
+
+    if (file.isDirectory()) {
+      return await walk(fullPath, tree);
+    } else {
+      return { ...tree, files: [...tree.files, fullPath] };
+    }
   });
+
+  const resultTree = { ...tree, dirs: [...tree.dirs, dir] };
+  const subTrees = await Promise.all(promises);
+
+  subTrees.forEach(({ dirs = [], files = [] }) => {
+    resultTree.dirs = [...resultTree.dirs, ...dirs];
+    resultTree.files = [...resultTree.files, ...files];
+  });
+
+  return resultTree;
 }
